571b50e1fe62726bdcfa0a5cfe160c42900580bb
Domin-MND <dominus.the.creator@gmail.com>
2023-03-08 18:30:46 +0300

    config fix, ikota version update
    added config line, ikota version 1.0.0 -> 1.0.1

--- example/src/component/component.ts
+++ example/src/component/component.ts
@@ -9,14 +9,17 @@ import { capitalCase } from '../utils/capitalCase';
  */
 export function component(config: IkotaConfig, name: string): string {
   // Here you can add a line to import a styling file
-  // Config file import line is imported automatically
-  // So are the types: FunctionComponent & ReactElement
+  // Types are imported automatically: FunctionComponent & ReactElement
 
   // You can use `` with multiple lines instead of an
   // array joined with \n, but for the sake of
   // indentation I left it this way
 
-  let response = `import classes from "./styles.module.styl";\n\n`;
+  let response = `import classes from "./styles.module.styl";\n`;
+  response += config.addConfigFile ? `import { buttonLabel } from "./config";\n` : "";
+
+  // Whitespace
+  response += "\n";
 
   // You may ignore the useLambdaSimplifier if you wish to
   if (config?.useLambdaSimplifier) {
